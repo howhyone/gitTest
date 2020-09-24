728b04cd92b1522b6f90d91206f1c0c2c3641e41
xu.zhang <xu.zhang@yintech.cn>
2020-09-24 19:32:42 +0800

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by fd-macmini on 2020/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
